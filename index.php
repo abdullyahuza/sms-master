0a5cde51e28c9f327c53a32475fb9a1a8ef35789
abdullyahuza <yahuzaabdulrazak@gmail.com>
2022-02-05 12:32:51 +0100

    updated index

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html dir="ltr" lang="en">
     <head>
         <meta charset="utf-8" />
-        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge"> -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- Tell the browser to be responsive to screen width -->
-        <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
-        <!-- <link href="./dist/css/style.min.css" rel="stylesheet"> -->
-        <!-- <link href="./assets/libs/toastr/build/toastr.min.css" rel="stylesheet"> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="./dist/css/style.min.css" rel="stylesheet" />
+        <link href="./assets/libs/toastr/build/toastr.min.css" rel="stylesheet" />
         <style type="text/css">
             body{
                 
