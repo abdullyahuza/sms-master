3e1c0c4d9fd82306065ef76838f98b3e22ecf509
abdullyahuza <yahuzaabdulrazak@gmail.com>
2022-02-05 12:29:42 +0100

    updated index

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Tell the browser to be responsive to screen width -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="./dist/css/style.min.css" rel="stylesheet">
+        <link href="./assets/libs/toastr/build/toastr.min.css" rel="stylesheet">
         
     </head>
     <body style="background-color: #1F262D;">
