fb2169b590c5c364ce6945eb9162f4ec8cd44727
abdullyahuza <yahuzaabdulrazak@gmail.com>
2022-02-05 12:30:25 +0100

    updated index

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!-- Tell the browser to be responsive to screen width -->
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="./dist/css/style.min.css" rel="stylesheet">
-        <link href="./assets/libs/toastr/build/toastr.min.css" rel="stylesheet">
         
+        <style type="text/css">
+            body{
+                
+            }
+            .links{
+                min-height:100vh;
+                transition: 0.5s;
+                transition-timing-function: ease-in;
+                background-color: black;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+        </style>
     </head>
     <body style="background-color: #1F262D;">
         <div class="preloader">
